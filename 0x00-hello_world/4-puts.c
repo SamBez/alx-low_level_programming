bad0a3f9293ecdd2f1bb0af7cdf006e628fee0dd
samrawit <gonesam77@gmail.com>
2022-04-07 16:23:09 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
+/**
+ *main - prints text
+ *Return: 0 if true
+ */
 int main(void)
 { 
-puts("\"Programming is like building a multilingual puzzle");
-	return (0); }
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
