3b66e67cb06f98d0de28b9f544ab6d0ec5ce889c
samrawit <gonesam77@gmail.com>
2022-04-09 14:17:50 -0700

    commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stio.h>
+#include <stdlib.h>
+/**
+ * main - mainf
+ * Return: 0 for func
+ */
+int main(void)
+{
+	char chC;
+	char chL;
+
+	for (chC = 'a'; chC <= 'z'; chC++)
+	{
+		putchar(chC);
+	}
+	for(chL = 'A'; chL <= 'Z'; chL++)
+	{
+		putchar(chL);
+	}
+	putchar('\n');
+	return (0);
+}
