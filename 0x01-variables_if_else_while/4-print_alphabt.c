5aee101fa5f136d56f5af0fe84b231c8b65bbc5b
samrawit <gonesam77@gmail.com>
2022-04-09 14:28:38 -0700

    commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - mainf
+ * Return: 0 for ll
+ */
+int main(void)
+{
+	int ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == 'e' || ch == 'q')
+		{
+			continue;
+		}
+		putchar(ch);
+	}
+	return (0);
+}
