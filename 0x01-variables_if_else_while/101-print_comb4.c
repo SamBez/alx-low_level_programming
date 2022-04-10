22d5be9671c8ba6790d0397a7ef309c1e77887a4
samrawit <gonesam77@gmail.com>
2022-04-10 07:35:48 -0700

    commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,27 +8,27 @@ int main(void)
 {
 	int x;
 	int y;  
-    int z;
+	int z;
 
 	for (x = 0; x < 9; x++)
 	{
 		y = x + 1;
 		while (y < 9)
 		{
-            z = y + 1;
-            while(z <= 9)
-            {
-                putchar(x + '0');
-                putchar(y + '0');
-                putchar(z + '0');
-			    z++;
-			    if (x == 7)
-			    {
-				    continue;
-			    }
-			    putchar(',');
-			    putchar(' ');
-            }
+			z = y + 1;
+			while(z <= 9)
+			{
+				putchar(x + '0');
+				putchar(y + '0');
+				putchar(z + '0');
+				z++;
+				if (x == 7)
+				{
+					continue;
+				}
+				putchar(',');
+				putchar(' ');
+			}
 			y++;   
 		}
 	}
