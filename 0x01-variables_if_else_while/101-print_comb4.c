c9cf1cf66a22c0e13f02fa2904702604f02e71d6
samri <gonesam77@gmail.com>
2022-04-10 16:52:54 +0300

    commit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - mainf
+ * Return: 0 for all
+ */
+int main(void)
+{
+	int x;
+	int y;
+    int z;
+
+	for (x = 0; x < 9; x++)
+	{
+		y = x + 1;
+
+		while (y < 9)
+		{
+            z = y + 1;
+            while(z <= 9)
+            {
+                putchar(x + '0');
+			    putchar(y + '0');
+                putchar(z + '0');
+			    z++;
+			    if (x == 7)
+			    {
+				    continue;
+			    }
+			    putchar(',');
+			    putchar(' ');
+            }
+			y++;   
+		}
+	}
+	putchar('\n');
+	return (0);
+}
