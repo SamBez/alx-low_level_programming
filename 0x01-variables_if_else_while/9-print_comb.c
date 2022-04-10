1285f72b7680876f0c74a32a39436a2598d909ba
samrawit <gonesam77@gmail.com>
2022-04-10 04:48:02 -0700

    commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - mianf
+ * Return: 0 for all
+ */
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x < 10: x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			putchar(x + '0');
+			putchar(',');
+			putchar(' ';
+		}
+	}
+	putchar('\n');
+	return (0);
+}
