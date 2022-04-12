a8411a53bd7a67bd1f5c4ff972aa927702e46683
samri <gonesam77@gmail.com>
2022-04-13 00:40:59 +0300

    commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - mianf
+ * Return: 0 for all
+ */
+int main(void)
+{
+    print_alphabet();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,3 +4,13 @@ static inline void _putchar(void)
     printf("%s", "_putchar");
     putchar('\n');
 }
+static inline void print_alphabet(void)
+{
+    char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+}
