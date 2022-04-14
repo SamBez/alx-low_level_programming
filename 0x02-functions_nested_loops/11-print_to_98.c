255d3274972a6965db5074d21f3a8afd9aa150d0
samrawit <gonesam77@gmail.com>
2022-04-13 18:11:53 -0700

    commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,13 +11,16 @@ void print_to_98(int n)
 	{
 		while (n > 98)
 			printf("%d, ", n--);
-		printf("%d\n", n);
+		printf("%d", n);
+		putchar('\n');
 	}
 
 	else
 	{
 		while (n < 98)
 			printf("%d, ", n++);
-		printf("%d\n", n);
+		printf("%d", n);
+		putchar('\n');
 	}
+
 }
