a42a1e9f311412e28ca6d9608e93df27f24dca5f
samri <gonesam77@gmail.com>
2022-04-16 01:48:24 +0300

    commit

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,17 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int c);
-int _abs(int n);
-int print_last_digit(int n);
-void jack_bauer(void);
-void times_table(void);
-int add(int num1, int num2);
-void print_to_98(int n);
-void print_times_table(int n);
+
+#include <stdio.h>
+
 void positive_or_negative(int i);
-#endif
+
+#endif /* MAIN_H */
