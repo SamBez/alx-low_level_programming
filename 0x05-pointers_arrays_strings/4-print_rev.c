75822ccb428aa5f773e459038ca69e1a2431b9e2
samrawit <gonesam77@gmail.com>
2022-04-20 14:18:06 -0700

    commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,12 +8,11 @@ void print_rev(char *s)
 {
 	int len = 0, index = 0, i;
 
-	while (s[index] != -1){
+	while (s[index] != -1)
+	{
 		len++;
-        index++;
-    }
-
-
+		index++;
+	}
 	for (i = len - 1; i >= 0; i--)
 		_putchar(s[i]);
 
