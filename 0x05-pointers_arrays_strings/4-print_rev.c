f2a98729e78a5589e7bc3f37710fc6fb9e5410c1
samri <gonesam77@gmail.com>
2022-04-21 00:15:47 +0300

    commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,13 +6,16 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int len = 0, index = 0, i;
 
-	while (s[index++])
+	while (s[index] != -1){
 		len++;
+        index++;
+    }
 
-	for (index = len - 1; index >= 0; index--)
-		_putchar(s[index]);
+
+	for (i = len - 1; i >= 0; i--)
+		_putchar(s[i]);
 
 	putchar('\n');
 }
