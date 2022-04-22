9d6b52f4ad743e232a14d66e8bfa545a9b80d929
samri <gonesam77@gmail.com>
2022-04-22 23:38:11 +0300

    commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,13 +15,11 @@ char *_strcat(char *dest, const char *src)
 	{
 		dlen++;
 	}
-
 	for (i = 0; src[i] != 0; i++)
 	{
 		dest[dlen] = src[i];
 		dlen++;
 	}
-
-	dest[dlen] = "\0";
+	dest[dlen] = '\0';
 	return (dest);
 }
