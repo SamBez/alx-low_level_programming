a74e0df0b6839de27d48bf2c9c3cb2deab090d74
samri <gonesam77@gmail.com>
2022-04-22 23:33:03 +0300

    commit

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -26,4 +26,6 @@ void print_triangle(int size);
 void print_number(int n);
 char *strcat(char *dest, const char *src);
 char *_strcat(char *dest, const char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 #endif
