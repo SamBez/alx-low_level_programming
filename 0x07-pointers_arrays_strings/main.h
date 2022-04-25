fff9531b1a4473db887159498cb5ef2dd203dc11
samri <gonesam77@gmail.com>
2022-04-26 00:17:48 +0300

    commit

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -36,4 +36,5 @@ char *leet(char *s);
 char *rot13(char *s);
 void print_number(int n);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 #endif
