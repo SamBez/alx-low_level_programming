70ef5f83aef223a886cd51b4b229d02131313023
samri <gonesam77@gmail.com>
2022-04-29 03:37:18 +0300

    commit

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -50,4 +50,6 @@ void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
+int sqrt2(int a, int b);
+int _sqrt_recursion(int n);
 #endif
